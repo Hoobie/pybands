0070e2e35411a25a077d66136d1f64e30227715b
Hoobie <hoobie92@gmail.com>
2015-01-08 00:31:26 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='pybands',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='MIT',
+    author='hoobie',
+    author_email='',
+    description='', requires=['numpy', 'matplotlib', 'pandas']
+)
